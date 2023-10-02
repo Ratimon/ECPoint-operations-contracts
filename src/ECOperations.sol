08d8b001dbc6cd8fa07a29615ec05bbd8f3baa58
Ratimon <ratixoxo@gmail.com>
2023-10-02 20:55:51 +0700

    Chore (./src): Cleaning unused code

--- src/ECOperations.sol
+++ src/ECOperations.sol
@@ -1,8 +1,6 @@
 //SPDX-License-Identifier: MIT
 pragma solidity =0.8.20;
 
-import {console2} from "@forge-std/console2.sol";
-
 
 /**
  * @notice leverages the precompiles
@@ -24,25 +22,18 @@ contract ECOperations {
         input[1] = A.y;
         input[2] = B.x;
         input[3] = B.y;
-        // bool success;
 
         ECPoint memory result;
 
         assembly {
             verified := staticcall(sub(gas(), 2000), 6, input, 0xc0, result, 0x60)
-          // Use "invalid" to make gas estimation work
             switch verified case 0 { invalid() }
           }
       
         require(verified, "add-failed");
-        // require(result == C, "result does not match");
         require(result.x == C.x, "result.x does not match");
         require(result.y == C.y, "result.y does not match");
 
-        console2.log("result.x: %s", result.x);
-        console2.log("result.y: %s", result.y);
-
-        // verified = success;
     }
 
 
